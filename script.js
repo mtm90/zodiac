8160db3d550c827036ba34965c4e47a10b8f893d
mtm90 <mattia22nchi@gmail.com>
2024-05-05 22:44:47 +0200

    removed birth time info

--- script.js
+++ script.js
@@ -5,64 +5,34 @@ birthForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
   const dob = new Date(document.getElementById("dob").value);
-  const time = document.getElementById("time").value.split(":");
-  const hours = parseInt(time[0]);
-  const minutes = parseInt(time[1]);
 
   const zodiacSign = calculateZodiacSign(dob.getMonth() + 1, dob.getDate());
   const chineseZodiacSign = calculateChineseZodiacSign(dob.getFullYear());
 
   resultDiv.innerHTML = `
-        <p>Zodiac Sign: ${zodiacSign}</p>
-        <p>Chinese Zodiac Sign: ${chineseZodiacSign}</p>
-      
+      <p>Zodiac Sign: ${zodiacSign}</p>
+      <p>Chinese Zodiac Sign: ${chineseZodiacSign}</p>
     `;
 });
 
 function calculateZodiacSign(month, day) {
   const zodiacSigns = [
-    "Aquarius",
-    "Pisces",
-    "Aries",
-    "Taurus",
-    "Gemini",
-    "Cancer",
-    "Leo",
-    "Virgo",
-    "Libra",
-    "Scorpio",
-    "Sagittarius",
-    "Capricorn",
+    "Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer",
+    "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn"
   ];
 
   const zodiacDates = [
-    [1, 20],
-    [2, 19],
-    [3, 21],
-    [4, 20],
-    [5, 21],
-    [6, 21],
-    [7, 23],
-    [9, 23],
-    [10, 23],
-    [11, 22],
-    [12, 22],
+    [1, 20], [2, 19], [3, 21], [4, 20], [5, 21], [6, 21],
+    [7, 23], [9, 23], [10, 23], [11, 22], [12, 22]
   ];
 
   for (let i = 0; i < zodiacDates.length; i++) {
     const startMonth = zodiacDates[i][0];
     const startDay = zodiacDates[i][1];
-    const endMonth = zodiacDates[i + 1]
-      ? zodiacDates[i + 1][0]
-      : zodiacDates[0][0];
-    const endDay = zodiacDates[i + 1]
-      ? zodiacDates[i + 1][1]
-      : zodiacDates[0][1];
+    const endMonth = zodiacDates[i + 1] ? zodiacDates[i + 1][0] : zodiacDates[0][0];
+    const endDay = zodiacDates[i + 1] ? zodiacDates[i + 1][1] : zodiacDates[0][1];
 
-    if (
-      (month === startMonth && day >= startDay) ||
-      (month === endMonth && day <= endDay)
-    ) {
+    if ((month === startMonth && day >= startDay) || (month === endMonth && day <= endDay)) {
       return zodiacSigns[i];
     }
   }
@@ -70,18 +40,8 @@ function calculateZodiacSign(month, day) {
 
 function calculateChineseZodiacSign(year) {
   const chineseZodiacs = [
-    "Rat",
-    "Ox",
-    "Tiger",
-    "Rabbit",
-    "Dragon",
-    "Snake",
-    "Horse",
-    "Goat",
-    "Monkey",
-    "Rooster",
-    "Dog",
-    "Pig",
+    "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake",
+    "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig"
   ];
 
   return chineseZodiacs[(year - 1900) % 12];
